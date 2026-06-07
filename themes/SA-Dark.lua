21491a833c0279b50c34420694312bcf27704993
Dieouwe <135071604+Die0uwe@users.noreply.github.com>
2026-06-07 22:17:15 +0200

    Add SA-Dark theme

---
+++ themes/SA-Dark.lua
@@ -0,0 +1,6 @@
+local theme={name="SA Dark",author="DieOuwe",description="Default Slayer Alliance dark purple theme",
+bg={0.04,0.02,0.08,0.97},bgSecondary={0.06,0.03,0.10,0.95},bgHover={0.12,0.05,0.20,1.0},
+border={0.25,0.07,0.40,1.0},borderAccent={0.55,0.15,0.85,1.0},borderHover={0.80,0.25,1.00,1.0},
+colorPrimary="|cffa335ee",colorSecondary="|cff00ccff",colorGold="|cffccaa00",colorGrey="|cff887799",
+tabActive={0.35,0.10,0.55,1.0},font="Fonts\\2002.ttf"}
+return theme
